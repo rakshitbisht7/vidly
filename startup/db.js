f295c364ac48fd0046ea32c76ebbd3f7a38faa7c
rakshitbisht7 <rakshitbisht1234@gmail.com>
2025-02-15 13:20:49 +0530

    debugging errors

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,11 @@ const config = require("config");
 
 module.exports = function () {
   const db = config.get("db");
+  console.log("MongoDB Connection String:", db); // Debug log
+
   mongoose
-    .connect(config.get("db"))
-    .then(() => console.log(`Connected to ${db}...`));
+    .connect(db)
+    .then(() => console.log(`Connected to ${db}...`))
+    .catch((err) => console.error("Could not connect to MongoDB...", err));
 };
+
