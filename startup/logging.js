9f4ba28ef509d9756970c3f0afea4cf6d415cd4a
rakshitbisht7 <rakshitbisht1234@gmail.com>
2025-02-15 14:30:52 +0530

    Fixed Winston transport issue and added config files

--- startup/logging.js
+++ startup/logging.js
@@ -2,21 +2,18 @@ const winston = require("winston");
 require("express-async-errors");
 
 module.exports = function () {
-  // Handle Uncaught Exceptions
-  winston.exceptions.handle(
-    new winston.transports.Console({ format: winston.format.simple() }),
-    new winston.transports.File({ filename: "uncaughtExceptions.log" })
-  );
+  // Prevent duplicate transports
+  if (!winston.transports.File) {
+    winston.exceptions.handle(
+      new winston.transports.Console({ format: winston.format.simple() }),
+      new winston.transports.File({ filename: "uncaughtExceptions.log" })
+    );
 
-  // Handle Unhandled Promise Rejections
-  process.on("unhandledRejection", (ex) => {
-    winston.error(ex.message, ex);
-    process.exit(1);
-  });
+    process.on("unhandledRejection", (ex) => {
+      winston.error(ex.message, ex);
+      process.exit(1);
+    });
 
-  // Add File Transport for Logging
-  winston.add(new winston.transports.File({ filename: "logfile.log" }));
-
-  // Optional: Add MongoDB Logging if needed
-  // winston.add(new winston.transports.MongoDB({ db: "mongodb://localhost/vidly", level: "error" }));
+    winston.add(new winston.transports.File({ filename: "logfile.log" }));
+  }
 };
