2cef7c0d8e372a2cc5555b823f7f2f357ad22336
rakshitbisht7 <rakshitbisht1234@gmail.com>
2025-02-15 15:04:46 +0530

    changed config

--- startup/config.js
+++ startup/config.js
@@ -1,8 +1,18 @@
-const config = require("config");
+// const config = require("config");
 
-module.exports = function(){
-    if (!config.get("jwtPrivateKey")) {
-        throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
-      }
+// module.exports = function(){
+//     if (!config.get("jwtPrivateKey")) {
+//         throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
+//       }
       
-}
+// }
+module.exports = function () {
+  const jwtPrivateKey = process.env.jwtPrivateKey || require("config").get("jwtPrivateKey");
+
+  console.log("JWT Private Key:", process.env.jwtPrivateKey ? "Loaded" : "Not Found");
+
+  if (!jwtPrivateKey) {
+    console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+    process.exit(1);
+  }
+};
